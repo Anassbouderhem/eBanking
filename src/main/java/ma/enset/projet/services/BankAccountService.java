858ae10f5e02bccc5ebb27d5a6f6722bb422faf9
Anass Bouderhem <as.bouderhem@gmail.com>
2026-05-08 12:51:32 +0200

    feat : add service layer

---
+++ src/main/java/ma/enset/projet/services/BankAccountService.java
@@ -0,0 +1,21 @@
+package ma.enset.projet.services;
+
+import ma.enset.projet.entities.BankAccount;
+import ma.enset.projet.entities.CurrentAccount;
+import ma.enset.projet.entities.Customer;
+import ma.enset.projet.entities.SavingAccount;
+import ma.enset.projet.exeptions.BankAccountNotFoundException;
+import ma.enset.projet.exeptions.CustomerNotFoundException;
+
+import java.util.List;
+
+public interface BankAccountService {
+    Customer saveCustomer(Customer customer);
+    CurrentAccount saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+    SavingAccount saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
+    void debit(String accountId, double amount, String description) throws BankAccountNotFoundException;
+    void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
+    void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BankAccountNotFoundException ;
+    List<Customer> listCustomers();
+    BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException;
+}
---
+++ src/main/java/ma/enset/projet/services/BankAccountServiceImpl.java
@@ -0,0 +1,120 @@
+package ma.enset.projet.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.enset.projet.entities.*;
+import ma.enset.projet.enums.AccountStatus;
+import ma.enset.projet.enums.OperationType;
+import ma.enset.projet.exeptions.BalanceNotSufficientException;
+import ma.enset.projet.exeptions.BankAccountNotFoundException;
+import ma.enset.projet.exeptions.CustomerNotFoundException;
+import ma.enset.projet.repositories.AccountOperationRepository;
+import ma.enset.projet.repositories.BankAccountRepository;
+import ma.enset.projet.repositories.CustomerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
+public class BankAccountServiceImpl implements BankAccountService{
+    AccountOperationRepository accountOperationRepository;
+    BankAccountRepository bankAccountRepository;
+    CustomerRepository customerRepository;
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        log.info("Saving Customer");
+       Customer savedCustomer = customerRepository.save(customer);
+        return savedCustomer;
+    }
+
+    @Override
+    public CurrentAccount saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException {
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+        if(customer==null)
+            throw new CustomerNotFoundException("Customer not found");
+        CurrentAccount currentAccount = new CurrentAccount();
+        currentAccount.setId(UUID.randomUUID().toString());
+        currentAccount.setCreatedAt(new Date());
+        currentAccount.setBalance(initialBalance);
+        currentAccount.setOverDraft(overDraft);
+        currentAccount.setStatus(AccountStatus.CREATED);
+        currentAccount.setCustomer(customer);
+        CurrentAccount savedBankAccount = bankAccountRepository.save(currentAccount);
+        return savedBankAccount;
+    }
+
+    @Override
+    public SavingAccount saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException {
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+        if(customer==null)
+            throw new CustomerNotFoundException("Customer not found");
+        SavingAccount savingAccount = new SavingAccount();
+        savingAccount.setId(UUID.randomUUID().toString());
+        savingAccount.setCreatedAt(new Date());
+        savingAccount.setBalance(initialBalance);
+        savingAccount.setInterestRate(interestRate);
+        savingAccount.setStatus(AccountStatus.CREATED);
+        savingAccount.setCustomer(customer);
+        SavingAccount savedBankAccount = bankAccountRepository.save(savingAccount);
+        return savedBankAccount;
+    }
+
+
+    @Override
+    public void debit(String accountId, double amount, String description) throws BankAccountNotFoundException {
+        BankAccount bankAccount = getBankAccount(accountId);
+        if(bankAccount.getBalance()<amount)
+            new BalanceNotSufficientException("Balance not sufficient");
+
+        AccountOperation accountOperation = new AccountOperation();
+        accountOperation.setType(OperationType.DEBIT);
+        accountOperation.setOperationDate(new Date());
+        accountOperation.setAmount(amount);
+        accountOperation.setDescription(description);
+        accountOperation.setBankAccount(bankAccount);
+        accountOperationRepository.save(accountOperation);
+        bankAccount.setBalance(bankAccount.getBalance()-amount);
+        bankAccountRepository.save(bankAccount);
+
+    }
+
+    @Override
+    public void credit(String accountId, double amount, String description) throws BankAccountNotFoundException {
+        BankAccount bankAccount = getBankAccount(accountId);
+        AccountOperation accountOperation = new AccountOperation();
+        accountOperation.setType(OperationType.CREDIT);
+        accountOperation.setOperationDate(new Date());
+        accountOperation.setAmount(amount);
+        accountOperation.setDescription(description);
+        accountOperation.setBankAccount(bankAccount);
+        accountOperationRepository.save(accountOperation);
+        bankAccount.setBalance(bankAccount.getBalance()+amount);
+        bankAccountRepository.save(bankAccount);
+
+    }
+
+    @Override
+    public void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BankAccountNotFoundException  {
+        debit(accountIdSource,amount,"Transfer to : " + accountIdDestination);
+        credit(accountIdDestination,amount,"Transfer from : " + accountIdSource);
+
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException {
+       BankAccount bankAccount = bankAccountRepository.findById(accountId).orElseThrow(()->new BankAccountNotFoundException("BankAccount not found"));
+       return bankAccount;
+    }
+}
